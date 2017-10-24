a509a8acf5a96cb22ba9d0d884e1f4da6f508fe7
Evgeniy <jason.ynk@gmail.com>
2017-10-24 11:34:10 +0300

    moveable lists done

--- src/common.js
+++ src/common.js
@@ -1,4 +1,50 @@
-var friendsList = document.querySelector('.list-friends');
+let friendsList = document.querySelector('.list-friends');
+let friendsChosenList = document.querySelector('.list-chosen-friends');
+let saveBtn = document.querySelector('.footer-save');
+let friendsArr = [];
+let friendsChosenArr = [];
+
+function saveStorage() {
+ 	 		localStorage.friendsList = JSON.stringify(friendsArr);
+ 	 		localStorage.friendsChosenList = JSON.stringify(friendsChosenArr); // Функцию с аргументами нельзя что-ли передавать? ????????????
+ 	 		alert('SAVED');
+ 	 }
+
+function renderFriendsInfo(arr, chosenArr) {
+	friendsList.textContent = '';
+	friendsChosenList.textContent = '';
+
+	for (let i = 0; i < arr.length; i++) {
+		let newLi = document.createElement('li');
+
+	//	arr.sort(function(item){
+	//		return item.first_name;
+	//	})
+
+		newLi.dataset.id = arr[i].id; 
+
+		let photo = arr[i].photo_100,
+			firstName = arr[i].first_name,
+			lastName = arr[i].last_name;
+
+ 		newLi.innerHTML = `<img class="face" src="${photo}" alt=""><p> ${firstName} ${lastName} </p> <img class="icon" src="img/add.png"></img>`;
+ 		friendsList.appendChild(newLi);
+	}
+
+	for (let i = 0; i < chosenArr.length; i++) {
+		let newLi = document.createElement('li');
+
+		newLi.dataset.id = chosenArr[i].id; 
+
+		let photo = chosenArr[i].photo_100,
+			firstName = chosenArr[i].first_name,
+			lastName = chosenArr[i].last_name;
+
+ 		newLi.innerHTML = `<img class="face" src="${photo}" alt=""><p> ${firstName} ${lastName} </p> <img class="icon" src="img/delete.png"></img>`;
+ 		friendsChosenList.appendChild(newLi);
+	}
+}
+
 function api(method, params) {
 	return new Promise(function(resolve, reject){
 		VK.api(method, params, function(data){
@@ -27,20 +73,62 @@ const p = new Promise(function(resolve, reject){
 
 });
 
- p
+p
  	.then(function(){
- 		return api('friends.get' , { v: 5.68, fields: 'nickname' });
+ 		return api('friends.get' , { v: 5.68, fields: 'nickname, photo_100' });
  	}) 
  	.then(function(data){
- 		for (let i = 0; i < data.items.length; i++) {
- 			var firstN = data.items[i].first_name;
- 			var lastN = data.items[i].last_name;
- 			var newLi = document.createElement('li');
- 			newLi.style = 'display: block';
- 			newLi.innerHTML = `<p> ${firstN} ${lastN} </p>`
- 			friendsList.appendChild(newLi);
- 			 console.log(data.items[0].first_name);
+ 		if('friendsList' in localStorage && 'friendsChosenList' in localStorage) {
+ 			let friendsStorageArr = JSON.parse(localStorage.friendsList),
+ 				friendsChosenStorageArr = JSON.parse(localStorage.friendsChosenList);
+ 				
+ 				for(let i = 0; i < friendsStorageArr.length; i++) {
+ 					friendsArr.push(friendsStorageArr[i]);
+ 				}
+
+ 				for(let i = 0; i < friendsChosenStorageArr.length; i++) {
+ 					friendsChosenArr.push(friendsChosenStorageArr[i]);
+ 				}
+
+ 				renderFriendsInfo(friendsArr, friendsChosenArr);
+
+ 		} else {
+ 			for (let i = 0; i < data.items.length; i++) {
+ 			friendsArr.push(data.items[i]);
+ 			}
+ 			renderFriendsInfo(friendsArr, friendsChosenArr);	
  		}
  		
  		
  	})
+ 	
+ 	friendsList.addEventListener('click', function(event) {
+ 		let target = event.target;
+
+ 		if(target.classList.contains('icon')) {
+ 			for(let i = 0; i < friendsArr.length; i++) {
+ 				 if (friendsArr[i].id === +target.parentElement.dataset.id) {
+ 				 	friendsChosenArr.push(friendsArr[i]);
+ 				 	friendsArr.splice(i, 1);
+ 				 }
+ 			}
+ 			renderFriendsInfo(friendsArr, friendsChosenArr);
+ 		}
+ 	});
+
+ 	friendsChosenList.addEventListener('click', function(event){
+ 		let target = event.target;
+
+ 		if(target.classList.contains('icon')) {
+ 			for(let i = 0; i < friendsChosenArr.length; i++) {
+ 				 if (friendsChosenArr[i].id === +target.parentElement.dataset.id) {
+ 				 	friendsArr.push(friendsChosenArr[i]);
+ 				 	friendsChosenArr.splice(i, 1);
+ 				 }
+ 			}
+ 			renderFriendsInfo(friendsArr, friendsChosenArr);
+ 		}
+ 	});
+
+
+ 	 saveBtn.addEventListener('click', saveStorage);
