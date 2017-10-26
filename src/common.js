d9efe20799c616b9ce2a4c10013d3f6005de9524
Evgeniy <jason.ynk@gmail.com>
2017-10-26 14:58:32 +0300

    filter done

--- src/common.js
+++ src/common.js
@@ -10,6 +10,8 @@ let friendsArr = [],
 	filteredArr = [],
 	filteredChosenArr = [];
 
+let dragObject = {};
+
 function saveStorage() {
  	localStorage.friendsList = JSON.stringify(friendsArr);
  	localStorage.friendsChosenList = JSON.stringify(friendsChosenArr); 
@@ -23,6 +25,7 @@ function renderFriendsInfo(arr, chosenArr) {
 	for (let i = 0; i < arr.length; i++) {
 		let newLi = document.createElement('li');
 		newLi.dataset.id = arr[i].id; 
+		newLi.setAttribute('class', 'draggable');
 
 		let photo = arr[i].photo_100,
 			firstName = arr[i].first_name,
@@ -34,6 +37,7 @@ function renderFriendsInfo(arr, chosenArr) {
 
 	for (let i = 0; i < chosenArr.length; i++) {
 		let newLi = document.createElement('li');
+		newLi.setAttribute('class', 'draggable');
 		newLi.dataset.id = chosenArr[i].id; 
 
 		let photo = chosenArr[i].photo_100,
@@ -43,6 +47,35 @@ function renderFriendsInfo(arr, chosenArr) {
  		newLi.innerHTML = `<img class="face" src="${photo}" alt=""><p> ${firstName} ${lastName} </p> <img class="icon" src="img/delete.png"></img>`;
  		friendsChosenList.appendChild(newLi);
 	}
+}
+
+function moveTo(arrFrom, arrTo, firstInput, secondInput, target) {
+	for (let i = 0; i < arrFrom.length; i++) {
+	 	if (arrFrom[i].id === +target.parentElement.dataset.id) {
+	 		arrTo.push(arrFrom[i]);
+	 		arrFrom.splice(i, 1);
+	 	}
+	}
+	fillFilteredArr(arrFrom, arrTo, firstInput, secondInput);
+}
+
+function fillFilteredArr (friendsArr, friendsChosenArr, firstInput, secondInput) {
+	filteredArr = [];
+	filteredChosenArr = [];
+
+	for (let i = 0; i < friendsArr.length; i++) {
+		if (isMatching(friendsArr[i].first_name, firstInput.value) || isMatching(friendsArr[i].last_name, firstInput.value)) {
+			filteredArr.push(friendsArr[i]);
+		}
+	}
+
+	for (let i = 0; i < friendsChosenArr.length; i++) {
+		if (isMatching(friendsChosenArr[i].first_name, secondInput.value) || isMatching(friendsChosenArr[i].last_name, secondInput.value)) {
+			filteredChosenArr.push(friendsChosenArr[i]);
+		}
+	}
+
+	renderFriendsInfo(filteredArr, filteredChosenArr);
 }
 
 function isMatching(full, chunk) {
@@ -67,7 +100,7 @@ const p = new Promise(function(resolve, reject){
 	});
 
 	VK.Auth.login(function(data){
-		if(data.session) {
+		if (data.session) {
 			resolve(data);
 		} else {
 			reject(new Error('Не удалось авторизироваться!'));
@@ -103,62 +136,129 @@ p
  	});
 
 friendsList.addEventListener('click', function(event) {
-	// function moveTo(arr, moveToArr) {
-    // 
-	// }
 	let target = event.target;
 
 	if (target.classList.contains('icon')) {
-		for (let i = 0; i < friendsArr.length; i++) {
-			 if (friendsArr[i].id === +target.parentElement.dataset.id) {
-			 	friendsChosenArr.push(friendsArr[i]);
-			 	friendsArr.splice(i, 1);
-			 }
-		}
-		renderFriendsInfo(friendsArr, friendsChosenArr);
+		moveTo(friendsArr, friendsChosenArr, filterInput, filterChosenInput, target);
+		renderFriendsInfo(filteredArr, filteredChosenArr);
 	}
 });
 
 friendsChosenList.addEventListener('click', function(event){
 	let target = event.target;
 
-	if(target.classList.contains('icon')) {
-		for(let i = 0; i < friendsChosenArr.length; i++) {
-			 if (friendsChosenArr[i].id === +target.parentElement.dataset.id) {
-			 	friendsArr.push(friendsChosenArr[i]);
-			 	friendsChosenArr.splice(i, 1);
-			 }
-		}
-		renderFriendsInfo(friendsArr, friendsChosenArr);
+	if (target.classList.contains('icon')) {
+		moveTo(friendsChosenArr, friendsArr, filterChosenInput, filterInput, target);
+		renderFriendsInfo(filteredChosenArr, filteredArr);
 	}
 });
 
 saveBtn.addEventListener('click', saveStorage);
 filterInput.addEventListener('input', (e) => {
-		filteredArr = [];
-		for(let i = 0; i < friendsArr.length; i++) {
-			if(isMatching(friendsArr[i].first_name, filterInput.value) || isMatching(friendsArr[i].last_name, filterInput.value)) {
-				filteredArr.push(friendsArr[i]);
-			}
-		}
-	 if(filterChosenInput.value) {
-	 	renderFriendsInfo(filteredArr, filteredChosenArr);
-	 } else {
-	 	renderFriendsInfo(filteredArr, friendsChosenArr);
-	 }
+	fillFilteredArr (friendsArr, friendsChosenArr, filterInput, filterChosenInput);
+
 });
 
 filterChosenInput.addEventListener('input', (e) => {
-		filteredChosenArr = [];
-		for(let i = 0; i < friendsChosenArr.length; i++) {
-			if(isMatching(friendsChosenArr[i].first_name, filterChosenInput.value) || isMatching(friendsChosenArr[i].last_name, filterChosenInput.value)) {
-				filteredChosenArr.push(friendsChosenArr[i]);
-			}
-		}
-		
-		if (filterInput.value) {
-			 renderFriendsInfo(filteredArr, filteredChosenArr);
-		} else {
-			renderFriendsInfo(friendsArr, filteredChosenArr);
-		}	
+	fillFilteredArr(friendsArr, friendsChosenArr, filterInput, filterChosenInput);
 });
+
+
+
+// document.addEventListener('mousedown', (e) => {
+// 	if (e.which != 1) { 
+//     	 return; 
+//   	}
+// 
+//   	let elem = e.target.closest('.draggable');
+//   	if (!elem) return;
+// 
+//   	dragObject.elem = elem;
+// 
+//  
+//   	dragObject.downX = e.pageX;
+//   	dragObject.downY = e.pageY;
+//   	console.log(dragObject);
+// 
+// });
+// 
+// document.addEventListener('mousemove', (e) => {
+// 	if (!dragObject.elem) return; 
+// 
+//   if ( !dragObject.avatar ) { // если перенос не начат...
+// 
+//     // посчитать дистанцию, на которую переместился курсор мыши
+//     var moveX = e.pageX - dragObject.downX;
+//     var moveY = e.pageY - dragObject.downY;
+//     if ( Math.abs(moveX) < 3 && Math.abs(moveY) < 3 ) {
+//       return; // ничего не делать, мышь не передвинулась достаточно далеко
+//     }
+// 
+//     dragObject.avatar = createAvatar(e); // захватить элемент
+//     if (!dragObject.avatar) {
+//       dragObject = {}; // аватар создать не удалось, отмена переноса
+//       return; // возможно, нельзя захватить за эту часть элемента
+//     }
+// 
+//     // аватар создан успешно
+//     // создать вспомогательные свойства shiftX/shiftY
+//     var coords = getCoords(dragObject.avatar);
+//     dragObject.shiftX = dragObject.downX - coords.left;
+//     dragObject.shiftY = dragObject.downY - coords.top;
+// 
+//     startDrag(e); // отобразить начало переноса
+//   }
+// 
+//   // отобразить перенос объекта при каждом движении мыши
+//   dragObject.avatar.style.left = e.pageX - dragObject.shiftX + 'px';
+//   dragObject.avatar.style.top = e.pageY - dragObject.shiftY + 'px';
+// 
+//   return false;
+// 
+// 
+// 
+// }); 
+//   
+// 
+//  
+// function createAvatar(e) {
+// 
+//   // запомнить старые свойства, чтобы вернуться к ним при отмене переноса
+//   var avatar = dragObject.elem;
+//   var old = {
+//     parent: avatar.parentNode,
+//     nextSibling: avatar.nextSibling,
+//     position: avatar.position || '',
+//     left: avatar.left || '',
+//     top: avatar.top || '',
+//     zIndex: avatar.zIndex || ''
+//   };
+// 
+//   // функция для отмены переноса
+//   avatar.rollback = function() {
+//     old.parent.insertBefore(avatar, old.nextSibling);
+//     avatar.style.position = old.position;
+//     avatar.style.left = old.left;
+//     avatar.style.top = old.top;
+//     avatar.style.zIndex = old.zIndex
+//   };
+// 
+//   return avatar;
+// }
+// 
+// function startDrag(e) {
+//   var avatar = dragObject.avatar;
+// 
+//   document.body.appendChild(avatar);
+//   avatar.style.zIndex = 9999;
+//   avatar.style.position = 'absolute';
+// }
+// 
+// 
+// function getCoords(elem) {   
+// 	var box = elem.getBoundingClientRect();
+// 	return {
+// 		top: box.top + pageYOffset,
+// 		left: box.left + pageXOffset
+// 	};
+// }
